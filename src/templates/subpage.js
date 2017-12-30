524dcbcdc23d08b399ce33a06a58c762a918386b
Erkka Pynnönen <erkka@blacklabel.fi>
2017-12-30 16:22:07 +0200

    Generate pages from contentful

--- src/templates/subpage.js
+++ src/templates/subpage.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import * as PropTypes from "prop-types"
 import Link from 'gatsby-link'
 
-const SubPage = () => (
-  <div>
-    <h1>Subpage Title</h1>
-    <p>Content here</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
-)
+const propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+class SubPage extends React.Component {
+  render() {
+    const page = this.props.data.contentfulPage
+    console.log(page)
+    return (
+      <div>
+        <h1>{page.title}</h1>
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: page.content.content,
+          }}
+        />
+
+        <div><Link to="/">Go back to the homepage</Link></div>
+      </div>
+    )
+  }
+}
 
 export default SubPage
+
+export const pageQuery = graphql`
+  query pageQuery( $id : String! ) {
+    contentfulPage( id: { eq: $id } ) {
+      id
+      title
+      content {
+        content
+      }
+    }
+  }
+`
